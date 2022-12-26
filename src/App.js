2e17ab564a2b9e40a3e2cd5a5ad33c8ea6ebeada
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-12-27 00:16:09 +0300

    Google Map Package Add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Wrapper apiKey={"AIzaSyD04A8KnsPhuz5EXgvDFUCDOUpx4rv0qyw"} />
+    </div>
+  );
 }
 
 export default App;
