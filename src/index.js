5dd62248761daefa275a8b71d11bbc488a870e17
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-12-27 11:32:52 +0300

    Google Maps Add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
